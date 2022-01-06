1e57a972bbc639a824588508f1220d502377b504
JY, Park <bleusoire@gmail.com>
2022-01-06 23:55:53 +0900

    init commit.

---
+++ src/main/java/dev/newv/gateway/NewvGatewayApplication.java
@@ -0,0 +1,14 @@
+package dev.newv.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class NewvGatewayApplication {
+
+    public static void main(String[] args) {
+
+        run(NewvGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/newv/gateway/NewvGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.newv.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewvGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
